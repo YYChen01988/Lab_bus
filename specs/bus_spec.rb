08ae956e90166e7db7548d6b68e9715102ef017b
yingying <chen.yingying.01988@gmail.com>
2018-07-24 16:30:17 +0100

    Refactors code

--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -6,9 +6,10 @@ require_relative("../bus_stop.rb")
 
 class BusTest < MiniTest::Test
   def setup
-    @person_1 = Person.new("Robert", 50)
+    @passenger_1 = Person.new("Robert", 50)
+    @passenger_2 = Person.new("Amy", 25)
     @bus_1 = Bus.new(22, "Ocean Terminal")
-    @bus_1.pick_up(@person_1)
+
   end
 
   def test_drive()
@@ -16,15 +17,16 @@ class BusTest < MiniTest::Test
   end
 
     def test_get_number_of_passengers()
-      assert_equal(1, @bus_1.passengers_count())
+      assert_equal(0, @bus_1.passengers_count())
     end
     def test_pick_up()
-      person_2 = Person.new("Amy", 25)
-      @bus_1.pick_up(person_2)
-      assert_equal(2, @bus_1.passengers_count())
+
+      @bus_1.pick_up(@person_2)
+      assert_equal(1, @bus_1.passengers_count())
     end
 
     def test_drop_off()
+      @bus_1.pick_up(@person_1)
       @bus_1.drop_off(@person_1)
       assert_equal(0, @bus_1.passengers_count())
     end
@@ -40,7 +42,7 @@ class BusTest < MiniTest::Test
       bus_stop_1.add_person_to_queue(person_in_queue)
       @bus_1.pick_up_from_stop(bus_stop_1)
       assert_equal(0, bus_stop_1.person_count())
-      assert_equal(2, @bus_1.passengers_count())
+      assert_equal(1, @bus_1.passengers_count())
 
     end
 
